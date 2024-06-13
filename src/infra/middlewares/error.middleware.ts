efd5cd5f1909658796710f9773a70bb33f9efb00
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-13 17:43:42 -0300

    chore: refactor erro middleware

--- src/infra/middlewares/error.middleware.ts
+++ src/infra/middlewares/error.middleware.ts
@@ -1,16 +1,14 @@
 import { NextFunction, Request, Response } from "express";
 import { HttpException } from "../../types/http.exception";
 
-export const ErrorMiddleware = (
+export const errorMiddleware = (
   err: HttpException,
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
-  return res
-    .status(err.status)
-    .json({
-      statusCode: err.status,
-      message: err.message || "Internal Server Error",
-    });
+  return res.status(err.status).json({
+    status: err.status,
+    message: err.message || "Internal Server Error",
+  });
 };
