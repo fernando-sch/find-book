56a54c78072e80e645c64243571cd6c9b4cd33e1
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-14 13:31:50 -0300

    feat: add get and update on book repository and abstracted repository

--- src/app/repositories/books.repository.ts
+++ src/app/repositories/books.repository.ts
@@ -1,7 +1,10 @@
+import { BookEntity } from "../../domain/entities/book.entity";
 import { BookDto } from "../dto/book.dto";
 
 // Abstraction of our Repository on infra layer
 // Because inner layer can't communicate directly with outer layers
 export abstract class BooksRepository {
   abstract create(dto: BookDto): void;
+  abstract find(dto: BookDto): Promise<BookEntity | null>;
+  abstract update(dto: BookDto, id: string): Promise<BookEntity | null>;
 }
