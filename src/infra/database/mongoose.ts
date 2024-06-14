681b512c20c6acc56569eb01882439928fc245eb
Fernando Schneider <fer.schneider.2000@gmail.com>
2024-06-14 10:58:39 -0300

    feat: add mongoose connect function

---
+++ src/infra/database/mongoose.ts
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+import { HttpException } from "../../types/http.exception";
+
+export const connect = async () => {
+  try {
+    if (!process.env.DATABASE_URL) {
+      throw new HttpException(500, "Missing DATABASE_URL");
+    }
+
+    await mongoose.connect(process.env.DATABASE_URL);
+    console.log("Database connected");
+  } catch (error: any) {
+    throw new HttpException(500, error.message);
+  }
+};
